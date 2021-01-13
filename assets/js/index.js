ef19d6d3f55957e4e2796f36b4d7d8a808a695d9
xc0de113acxx <77364488+xc0de113acxx@users.noreply.github.com>
2021-01-13 15:35:48 -0800

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,8 @@
 var body = document.getElementsByTagName("body")[0];
 var documentBody = document.createElement("div");
 body.appendChild(documentBody);
+
+var title = document.createElement("div");
+title.className = "page-title";
+title.innerHTML = "CoolCraft: Reloaded";
+documentBody.appendChild(title);
